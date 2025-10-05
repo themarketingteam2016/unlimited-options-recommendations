50286f0cc9be35f6f37dd7447b55916179a3230d
Shopify App Developer <noreply@shopify.app>
2025-10-05 20:17:51 +0530

    Fix: Add missing Link import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
